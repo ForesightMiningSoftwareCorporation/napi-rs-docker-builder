57bce1205d4945e357f47e9343987b501bc00a63
Loïs Postula <lois@postu.la>
2022-11-18 08:56:06 +0100

    [enh] ensure gh actions ssh agent is working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 
+cp -R /ssh/gitconfig ~/.gitconfig
+cp -R /ssh/.ssh ~/.ssh
+
+chown root:root ~/.gitconfig
+chown -R root:root ~/.ssh
+
+sed 's|/home/runner|/root|g' -i.bak ~/.ssh/config
+
+RUN echo -e '\n[url "ssh://git@github.com/ForesightMiningSoftwareCorporation"]\n  insteadOf = https://github.com/ForesightMiningSoftwareCorporation' >> /root/.gitconfig
+
+mkdir -p -m 0600 ~/.ssh && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+# This will exec the CMD from your Dockerfile, i.e. "npm start"
+exec "$@"
