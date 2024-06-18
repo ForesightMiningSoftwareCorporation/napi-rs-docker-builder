da16081dbde406adc1fc59b6b8d801e7820de97f
Loïs Postula <lois@postu.la>
2024-06-18 07:08:00 +0200

    fix: fix shipyard host key

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,10 @@ chown -R root:root ~/.ssh
 
 sed 's|/home/runner|/root|g' -i.bak ~/.ssh/config
 
-echo -e '\n[url "ssh://git@github.com"]\n  insteadOf = https://github.com' >> /root/.gitconfig
+echo -e '\n[url "ssh://git@github.com/ForesightMiningSoftwareCorporation"]\n  insteadOf = https://github.com/ForesightMiningSoftwareCorporation' >> /root/.gitconfig
+echo -e '\n[url "ssh://git@github.com/foresightminingsoftwarecorporation"]\n  insteadOf = https://github.com/foresightminingsoftwarecorporation' >> /root/.gitconfig
 
-mkdir -p -m 0600 ~/.ssh && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+mkdir -p -m 0600 ~/.ssh && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts && ssh-keyscan -t rsa ssh.shipyard.rs >> ~/.ssh/known_hosts
 
 # This will exec the CMD from your Dockerfile, i.e. "npm start"
 exec "$@"
